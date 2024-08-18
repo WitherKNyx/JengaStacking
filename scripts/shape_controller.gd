254e93afd1dee62365f85e721ec98767d57f67d5
Nyx Gonzalez <nyxgonz@gmail.com>
2024-08-17 21:36:44 -0400

    Modification to pos logic

--- scripts/shape_controller.gd
+++ scripts/shape_controller.gd
@@ -27,20 +27,16 @@ func _physics_process(_delta: float) -> void:
 func placing_shape() -> void:
 	var shape_to_place: Resource = load("res://scenes/square_2.tscn")
 	shape_instance = shape_to_place.instantiate()
-	
-	var x_pos: float = get_global_mouse_position().x
-	var ctrans: Transform2D = get_canvas_transform()
-	var y_pos: float = (-ctrans.get_origin() / ctrans.get_scale()).y
-	var rounded_position: Vector2 = Vector2(x_pos, y_pos)
-	shape_instance.global_position = rounded_position
-	
+	shape_instance.global_position = shape_init_pos()
 	add_child(shape_instance)
 
-func update_shape_pos() -> void:
+func shape_init_pos() -> Vector2:
 	var x_pos: float = get_global_mouse_position().x
 	var ctrans: Transform2D = get_canvas_transform()
 	var y_pos: float = (-ctrans.get_origin() / ctrans.get_scale()).y
-	var rounded_position: Vector2 = Vector2(x_pos, y_pos)
+	return Vector2(x_pos, y_pos)
+
+func update_shape_pos() -> void:
 	if (shape_instance != null):
-		shape_instance.global_position = rounded_position
+		shape_instance.global_position = shape_init_pos()
 		shape_instance.move_and_collide(Vector2(0, 2000))
